b5239c6e10b1c532d36f498da34194bef00adce9
Kungger-git <matthewriel63@gmail.com>
2021-11-10 09:25:58 +0800

    fixed automating polybar modules

--- etc/skel/.config/bspwm/polybar/launch.sh
+++ etc/skel/.config/bspwm/polybar/launch.sh
@@ -1,11 +1,57 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Terminate already running bar instances
-killall -q polybar
+## Files and Directories
+DIR="$HOME/.config/bspwm/polybar"
+SFILE="$DIR/system"
+RFILE="$DIR/.system"
+MFILE="$DIR/.module"
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+## Get system variable values for various modules
+get_values() {
+	CARD=$(basename "$(find /sys/class/backlight/* | head -n 1)")
+	BATTERY=$(basename "$(find /sys/class/power_supply/*BAT* | head -n 1)")
+	ADAPTER=$( "$(find /sys/class/power_supply/*AC* | head -n 1)")
+	INTERFACE=$(ip link | awk '/state UP/ {print $2}' | tr -d :)
+}
 
-polybar -c ~/.config/bspwm/polybar/config -rq bar &
+## Write values to `system` file
+set_values() {
+	if [[ "$ADAPTER" ]]; then
+		sed -i -e "s/adapter = .*/adapter = $ADAPTER/g" "$SFILE"
+	fi
+	if [[ "$BATTERY" ]]; then
+		sed -i -e "s/battery = .*/battery = $BATTERY/g" "$SFILE"
+	fi
+	if [[ "$CARD" ]]; then
+		sed -i -e "s/graphics_card = .*/graphics_card = $CARD/g" "$SFILE"
+	fi
+	if [[ "$INTERFACE" ]]; then
+		sed -i -e "s/network_interface = .*/network_interface = $INTERFACE/g" "$SFILE"
+	fi
+}
 
-echo "Polybar launched..."
+## Launch Polybar with selected style
+launch_bar() {
+	CARD=$(basename "$(find /sys/class/backlight/* | head -n 1)")
+	if [[ "$CARD" != *"intel_"* ]]; then
+		if [[ ! -f "$MFILE" ]]; then
+			sed -i -e 's/backlight/brightness/g' "$DIR"/config
+			touch "$MFILE"
+		fi
+	fi
+		
+	if [[ ! $(pidof polybar) ]]; then
+		polybar -q bar -c "$DIR"/config &
+	else
+		polybar-msg cmd restart
+	fi
+}
+
+# Execute functions
+if [[ ! -f "$RFILE" ]]; then
+	get_values
+	set_values
+	touch "$RFILE"
+fi
+
+launch_bar
